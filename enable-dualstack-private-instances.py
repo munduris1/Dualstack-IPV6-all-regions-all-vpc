b531bf13eadf4e5700ea4231267619b516b70fc4
munduris1 <60046292+munduris1@users.noreply.github.com>
2024-07-12 10:52:32 -0400

    Update enable-dualstack-private-instances.py

--- enable-dualstack-private-instances.py
+++ enable-dualstack-private-instances.py
@@ -1,21 +1,16 @@
 import boto3
+import json
+import logging
+from botocore.config import Config
+from ipaddress import IPv6Network
 
-def enable_ipv6_on_vpc(ec2_client, vpc_id):
-    # Associate an IPv6 CIDR block with the VPC
-    response = ec2_client.associate_vpc_cidr_block(
-        VpcId=vpc_id,
-        AmazonProvidedIpv6CidrBlock=True
-    )
-    print(f"Enabled IPv6 on VPC {vpc_id}")
-    return response['Ipv6CidrBlockAssociation']['Ipv6CidrBlock']
-def assign_ipv6_to_subnet(ec2_client, subnet_id, ipv6_cidr_block):
-    # Associate an IPv6 CIDR block with the subnet
-    response = ec2_client.associate_subnet_cidr_block(
-        SubnetId=subnet_id,
-        Ipv6CidrBlock=f"{ipv6_cidr_block[:-1]}::{int(subnet_id[-1], 16):x}/64"
-    )
-    print(f"Assigned IPv6 CIDR block to subnet {subnet_id}")
-    return response['Ipv6CidrBlock']
+# Setup logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s [%(process)s] [%(levelname)s] [%(funcName)s] %(message)s')
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 def assign_ipv6_addresses_to_instances(ec2_client, subnet_id):
     # Describe instances in the subnet
@@ -37,7 +32,14 @@ def assign_ipv6_addresses_to_instances(ec2_client, subnet_id):
                     )
                     print(f"Assigned IPv6 address to instance {instance['InstanceId']}")
 
-def main():
+def lambda_handler(event, context):
+    """
+    Main Lambda handler function to enable IPv6 for private instances.
+    
+    Parameters:
+        event: Event data.
+        context: Runtime information.
+    """
     session = boto3.Session()
     ec2 = session.client('ec2')
     
@@ -54,41 +56,36 @@ def main():
         vpcs_response = ec2_client.describe_vpcs()
         for vpc in vpcs_response['Vpcs']:
             vpc_id = vpc['VpcId']
-            #if vpc_id in "vpc-08bd2cb875fa89b38":
-            # Check if the VPC has IPv6 enabled
-            if 'Ipv6CidrBlockAssociationSet' not in vpc or not vpc['Ipv6CidrBlockAssociationSet']:
-                print(f"vpc is not assigned IPv6 {vpc['VpcId']}")
-                continue  # Skip VPCs without IPv6 enabled
-
-            # List all subnets in the VPC
-            subnets_response = ec2_client.describe_subnets(
-                Filters=[{'Name': 'vpc-id', 'Values': [vpc_id]}]
-            )
-
-            for subnet in subnets_response['Subnets']:
-                subnet_id = subnet['SubnetId']
-                # Check if the subnet is private and has IPv6 enabled
-                route_table_response = ec2_client.describe_route_tables(
-                Filters=[{'Name': 'association.subnet-id', 'Values': [subnet_id]}]
+            if vpc_id in "vpc-08bd2cb875fa89b38":
+                # Check if the VPC has IPv6 enabled
+                if 'Ipv6CidrBlockAssociationSet' not in vpc or not vpc['Ipv6CidrBlockAssociationSet']:
+                    print(f"vpc is not assigned IPv6 {vpc['VpcId']}")
+                    continue  # Skip VPCs without IPv6 enabled
+    
+                # List all subnets in the VPC
+                subnets_response = ec2_client.describe_subnets(
+                    Filters=[{'Name': 'vpc-id', 'Values': [vpc_id]}]
                 )
-                if route_table_response['RouteTables']:
-                    route_table = route_table_response['RouteTables'][0]
-
-                    # Check if the subnet is private by looking for the absence of an internet gateway route
-                    igw_route = any(route.get('GatewayId', '').startswith('igw-') for route in route_table['Routes'])
-                    if igw_route :
-                        continue
-                    
+    
+                for subnet in subnets_response['Subnets']:
+                    subnet_id = subnet['SubnetId']
                     # Check if the subnet is private and has IPv6 enabled
-                    #if subnet['MapPublicIpOnLaunch']:
-                        #continue  # Skip public subnets
-
-                    if 'Ipv6CidrBlockAssociationSet' not in subnet or not subnet['Ipv6CidrBlockAssociationSet']:
-                        print(f"Subnet is not enabled for ipv6 {subnet_id}")
-                        continue  # Skip subnets without IPv6 enabled
-
-                    # Assign IPv6 addresses to instances in the subnet
-                    assign_ipv6_addresses_to_instances(ec2_client, subnet_id)
-
-if __name__ == "__main__":
-    main()
+                    route_table_response = ec2_client.describe_route_tables(
+                    Filters=[{'Name': 'association.subnet-id', 'Values': [subnet_id]}]
+                    )
+                    if route_table_response['RouteTables']:
+                        route_table = route_table_response['RouteTables'][0]
+    
+                        # Check if the subnet is private by looking for the absence of an internet gateway route
+                        igw_route = any(route.get('GatewayId', '').startswith('igw-') for route in route_table['Routes'])
+                        if igw_route :
+                            continue
+                        
+                       
+    
+                        if 'Ipv6CidrBlockAssociationSet' not in subnet or not subnet['Ipv6CidrBlockAssociationSet']:
+                            print(f"Subnet is not enabled for ipv6 {subnet_id}")
+                            continue  # Skip subnets without IPv6 enabled
+    
+                        # Assign IPv6 addresses to instances in the subnet
+                        assign_ipv6_addresses_to_instances(ec2_client, subnet_id)
